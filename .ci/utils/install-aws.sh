1a44e30b091d994830c867b6b3ee30efbbbaa9b3
James Newell <james@digitaledgeit.com.au>
2019-03-20 02:26:29 +1100

    install AWS CLI

--- .ci/utils/install-aws.sh
+++ .ci/utils/install-aws.sh
@@ -1,5 +1,5 @@
 # install AWS CLI
 # TODO: create a docker image in order to speed this up
-RUN sudo apt-get update && \
-    sudo apt-get install python-pip && \
-    sudo pip install awscli
+sudo apt-get update && \
+  sudo apt-get install python-pip && \
+  sudo pip install awscli
