d1e65afffc612dac4c0eef872506fa9d8644a223
James Newell <james@jameslnewell.dev>
2020-09-15 03:01:28 +1000

    added opengraph metadata

--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import {Helmet} from 'react-helmet';
 import {Reset} from '../../ui';
 import favicon from '!file-loader!./img/favicon.png';
+import james from '!file-loader!../../images/james.jpg';
 
+// FIXME: make URL dynamic
+const url = 'https://jameslnewell.dev/';
 const bio = `An 🇦🇺 Software Engineer passionate about empowering teams to deliver more efficiently, with more confidence and to a higher degree of quality.`;
 
 export const Root: React.FC = ({children}) => (
@@ -12,8 +15,12 @@ export const Root: React.FC = ({children}) => (
       <title>{bio}</title>
       <meta name="description" content={bio} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="og:title" content="James Newell &mdash; Software Engineer" />
+      <meta name="og:description" content={bio} />
+      <meta name="og:image" content={james} />
+      <meta name="og:url" content={url} />
       <link rel="icon" type="image/png" sizes="32x32" href={favicon} />
-      <link rel="canonical" href="https://jameslnewell.dev/" />
+      <link rel="canonical" href={url} />
     </Helmet>
     <Reset />
     {children}
